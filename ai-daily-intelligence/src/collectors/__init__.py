419e4e550f9a8b915ab6a69c1eff014ec2a70014
ericx985 <zscvh5ps8j@privaterelay.appleid.com>
2026-08-23 21:15:28 +0800

    Delete ai-daily-intelligence directory

--- ai-daily-intelligence/src/collectors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""数据采集器模块"""
-from .arxiv import ArxivCollector
-from .github import GitHubCollector
-from .hackernews import HNCollector
-from .rss import RSSCollector
-
-__all__ = ["ArxivCollector", "GitHubCollector", "HNCollector", "RSSCollector"]
